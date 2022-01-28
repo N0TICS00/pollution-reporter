0679c050dd515c853b7236fef347b69a18fbfba8
N0TICS0 <74853755+N0TICS0@users.noreply.github.com>
2022-01-28 16:02:32 +0100

    Added Form

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let express = require("express")
+let bodyParser = require("body-parser")
 let app = express()
-
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.set('view engine' , 'ejs' )
 app.use('/assets/', express.static("assets"))
 app.get("/",(req,res) => {
  
     res.render("index")
     
+})
+app.post("/", urlencodedParser, (res,req)=>{
+    let city = req.body
+    console.log(city)
 })
 
 
