42855fa6faaf0ba3051bd5086c8a48e9244c9f47
N0TICS0 <74853755+N0TICS0@users.noreply.github.com>
2022-01-29 15:48:50 +0100

    Added MySQL

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+//REQUIRE
 let express = require("express")
 let bodyParser = require("body-parser")
+let mysql = require("mysql")
+//SERVER
+let connection = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password : "",
+    database : "pollution-reporter"
+})
+connection.connect()
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+  });
+  connection.end();
+
 let app = express()
 let urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.set('view engine' , 'ejs' )
@@ -9,9 +25,9 @@ app.get("/",(req,res) => {
     res.render("index")
     
 })
-app.post("/", urlencodedParser, (res,req)=>{
+app.post("/", urlencodedParser, (req,res)=>{
     let city = req.body
-    console.log(city)
+    res.render("index", city)
 })
 
 
